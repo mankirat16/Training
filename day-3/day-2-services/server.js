dfab9a5743732b587a7bbc6db60e92db0330aecc
mankirat16 <mankirat0816@gmail.com>
2024-06-13 12:04:13 +0530

    implement session managemet without using express-session

--- day-3/day-2-services/server.js
+++ day-3/day-2-services/server.js
@@ -1,3 +1,76 @@
+// require("dotenv").config();
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const PORT = process.env.PORT || 5000;
+// const app = express();
+// const cors = require("cors");
+// const session = require("express-session");
+// const cookieParser = require("cookie-parser");
+// const path = require("path");
+// app.use(cors());
+// app.use(express.urlencoded());
+// app.use(express.static("."));
+// app.use(express.json());
+// app.use(cookieParser());
+// app.use(express.static("./public"));
+
+// app.use(
+//   session({
+//     saveUninitialized: true,
+//     resave: false,
+//     secret: "asdf1323f3f2f",
+//     cookie: { maxAge: 400000 },
+//   })
+// );
+// mongoose.connect(process.env.DB_URL).then((res) => {
+//   console.log("connected");
+// });
+// const db = require("./models/users");
+// app.get("/", (req, res) => {
+//   if (req.session.username) res.redirect("/dashboard   ");
+//   res.sendFile(path.join(__dirname, "public/login.html"));
+// });
+// app.get("/dashboard", (req, res) => {
+//   if (req.session.name)
+//     res.sendFile(path.join(__dirname, "public/dashboard.html"));
+//   else res.redirect("/");
+// });
+// app.post("/login", async (req, res) => {
+//   const user = await db.find({ name: req.body.name }, { pwd: req.body.pwd });
+//   if (user.length === 0) res.send("Invalid username or password");
+//   else {
+//     req.session.name = req.body.name;
+//     res.redirect("/dashboard");
+//   }
+// });
+// app.post("/signup", async (req, res) => {
+//   const user = {};
+//   user.name = req.body.name;
+//   user.pwd = req.body.pwd;
+//   const oldUser = await db.find({ name: req.body.name });
+//   if (oldUser.length != 0) {
+//     res.redirect("/");
+//   } else {
+//     const userMade = await db.create(user);
+//     if (userMade) {
+//       req.session.name = req.body.name;
+//       res.redirect("/dashboard");
+//     }
+//   }
+// });
+// app.get("/logout", (req, res) => {
+//   req.session.destroy();
+//   res.redirect("/");
+// });
+// app.listen(PORT, (err) => {
+//   if (!err) console.log(`server running on port ${PORT}...`);
+//   else {
+//     console.log(err);
+//   }
+// });
+
+//--------------------------------------------> using cookies i.e without using express-session package
+
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
@@ -27,11 +100,10 @@ mongoose.connect(process.env.DB_URL).then((res) => {
 });
 const db = require("./models/users");
 app.get("/", (req, res) => {
-  if (req.session.username) res.redirect("/dashboard   ");
   res.sendFile(path.join(__dirname, "public/login.html"));
 });
 app.get("/dashboard", (req, res) => {
-  if (req.session.name)
+  if (req.cookies.user)
     res.sendFile(path.join(__dirname, "public/dashboard.html"));
   else res.redirect("/");
 });
@@ -39,7 +111,7 @@ app.post("/login", async (req, res) => {
   const user = await db.find({ name: req.body.name }, { pwd: req.body.pwd });
   if (user.length === 0) res.send("Invalid username or password");
   else {
-    req.session.name = req.body.name;
+    res.cookie("user", req.body.name);
     res.redirect("/dashboard");
   }
 });
@@ -49,17 +121,18 @@ app.post("/signup", async (req, res) => {
   user.pwd = req.body.pwd;
   const oldUser = await db.find({ name: req.body.name });
   if (oldUser.length != 0) {
+    res.cookie("user", req.body.name);
     res.redirect("/");
   } else {
+    res.cookie("user", req.body.name);
     const userMade = await db.create(user);
     if (userMade) {
-      req.session.name = req.body.name;
       res.redirect("/dashboard");
     }
   }
 });
 app.get("/logout", (req, res) => {
-  req.session.destroy();
+  res.clearCookie("user");
   res.redirect("/");
 });
 app.listen(PORT, (err) => {
