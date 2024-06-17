1943b65435820b204f3d768ebc1293ae7a7eb5de
mankirat16 <mankirat0816@gmail.com>
2024-06-17 14:25:42 +0530

    add code for day 5

---
+++ day-5/server.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+const express = require("express");
+const PORT = process.env.PORT || 5000;
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+app.use(cors());
+app.use(express.urlencoded());
+app.use(express.static("."));
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static("./public"));
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads");
+  },
+  filename: (req, file, cb) => {
+    const name = file.originalname;
+    cb(null, name);
+  },
+});
+const upload = multer({ storage: storage });
+const secret = process.env.TOKEN_SECRET;
+mongoose.connect(process.env.DB_URL).then((res) => {
+  console.log("connected");
+});
+const db = require("./models/users");
+app.post("/upload", upload.single("image"), (req, res) => {
+  console.log(req.file);
+  res.send("hello");
+});
+app.listen(PORT, (err) => {
+  if (!err) console.log(`server running on port ${PORT}...`);
+  else {
+    console.log(err);
+  }
+});
+ 
